3844ec0ad1510cdeaed16a34d557f71c0126864f
Dacey Nolan <54158919+daceynolan@users.noreply.github.com>
2019-10-10 19:34:47 -0500

    Integrate lightbox component
    * Integrated lightbox component
    
    * Renamed variables to something more descriptive
    
    * Fix self closing tag

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { InfiniteScroll } from "react-simple-infinite-scroll";
+import Carousel, { Modal, ModalGateway } from "react-images";
 import Gallery from "react-photo-gallery";
 
 import { generatePhotoUrl, loadPhoto } from "../utils/photos";
@@ -10,6 +11,8 @@ const PHOTOS_PER_PAGE = 20;
 const App = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
+  const [lightboxIsOpen, setLightboxIsOpen] = useState(false);
+  const [currentPhotoIndex, setCurrentPhotoIndex] = useState(0);
 
   // Fetch initial photos
   useEffect(() => {
@@ -28,17 +31,26 @@ const App = () => {
       .map((_, i) => {
         const newPhotoIndex = currentPhotoCount + 1 + i;
         if (newPhotoIndex < TOTAL_PHOTO_COUNT) {
-          return generatePhotoUrl(currentPhotoCount + 1 + i);
+          return generatePhotoUrl(currentPhotoCount + i);
         }
         return null;
       })
       .filter(Boolean);
-
     // Wait for all promises to resolve
     Promise.all(nextPhotoUrls.map(loadPhoto)).then(newPhotos => {
       setIsLoading(false);
       setPhotos(photos.concat(newPhotos));
     });
+  };
+
+  const openLightbox = useCallback((event, { index }) => {
+    setCurrentPhotoIndex(index);
+    setLightboxIsOpen(true);
+  }, []);
+
+  const closeLightBox = () => {
+    setCurrentPhotoIndex(0);
+    setLightboxIsOpen(false);
   };
 
   return (
@@ -50,8 +62,24 @@ const App = () => {
         hasMore={photos.length < TOTAL_PHOTO_COUNT}
         onLoadMore={loadPhotos}
       >
-        <Gallery photos={photos} />
+        <Gallery photos={photos} onClick={openLightbox} />
       </InfiniteScroll>
+      <ModalGateway>
+        {lightboxIsOpen ? (
+          <Modal onClose={closeLightBox}>
+            <Carousel
+              currentIndex={currentPhotoIndex}
+              views={photos.map((photo, i) => ({
+                ...photo,
+                source: {
+                  thumbnail: photo.src,
+                  regular: generatePhotoUrl(i, "large")
+                }
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
     </div>
   );
 };
