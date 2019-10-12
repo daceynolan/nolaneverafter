51ff2c7cf08d70770a59553b0b6ca70933b8630b
Dacey Nolan <54158919+daceynolan@users.noreply.github.com>
2019-10-12 18:42:31 -0500

    Display loader while fetching photos
    * Display loader while fetching photos
    
    * Remove duplicate input and restore number loading per page

---
+++ src/components/Loader.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// https://loading.io/css/
+const Loader = () => {
+  return (
+    <div class="lds-heart">
+      <div></div>
+    </div>
+  );
+};
+
+export default Loader;
