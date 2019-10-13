301ef61511b691b59ef479fa2ad760c1b12b4d45
Dacey Nolan <daceyshirley@gmail.com>
2019-10-12 21:11:35 -0500

    Edit fill prop

--- src/components/CloseIcon.js
+++ src/components/CloseIcon.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default () => (
-  <svg className="icon" viewBox="0 0 24 24">
+  <svg fill="currentColor" viewBox="0 0 24 24">
     <path d="M18.984 6.422l-5.578 5.578 5.578 5.578-1.406 1.406-5.578-5.578-5.578 5.578-1.406-1.406 5.578-5.578-5.578-5.578 1.406-1.406 5.578 5.578 5.578-5.578z" />
   </svg>
 );
