ee347ed18bbb77ea1abde03c5493ec24f7dd5ea4
Dacey Nolan <54158919+daceynolan@users.noreply.github.com>
2019-10-12 21:36:08 -0500

    Add footer component
    * Add footer component
    
    * Delete unnecessary spaces

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">Website made with love by the Bride and Groom!</div>
+  );
+};
+
+export default Footer;
